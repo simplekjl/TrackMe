eaff36452d8f0c229e5ce53e583007abf07daf5a
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-16 11:37:07 +0200

    Added maps lib (#2)

--- buildSrc/src/main/java/Google.kt
+++ buildSrc/src/main/java/Google.kt
@@ -3,6 +3,7 @@ object Google {
     private const val materialVersion = "1.4.0"
     private const val daggerVersion = "2.42"
     private const val hiltVersion = "2.38.1"
+    private const val mapsVersion = "18.1.0"
 
     const val material = "com.google.android.material:material:$materialVersion"
     const val daggerCore = "com.google.dagger:dagger:$daggerVersion"
@@ -13,5 +14,8 @@ object Google {
     const val hiltClasspath = "com.google.dagger:hilt-android-gradle-plugin:$hiltVersion"
     const val hiltAndroid = "com.google.dagger:hilt-android:$hiltVersion"
     const val hiltCompiler = "com.google.dagger:hilt-android-compiler:$hiltVersion"
+
+    // MAPS
+    const val maps = "com.google.android.gms:play-services-maps:$mapsVersion"
 
 }
