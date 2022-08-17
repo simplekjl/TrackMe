1bc744c5f4ee09911a02be8a847eaeeab4592b47
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-17 18:24:58 +0200

    updating view and libs

--- app/src/main/kotlin/com/simplekjl/trackme/di/Module.kt
+++ app/src/main/kotlin/com/simplekjl/trackme/di/Module.kt
@@ -3,8 +3,8 @@ package com.simplekjl.trackme.di
 import com.simplekjl.data.client.FlickrService
 import com.simplekjl.data.repository.NetworkSource
 import com.simplekjl.trackme.BuildConfig
-import com.simplekjl.trackme.framework.DistanceTrackerService
 import com.simplekjl.trackme.framework.RepositoriesSource
+import com.simplekjl.trackme.ui.trackimages.TrackingViewModel
 import java.util.concurrent.TimeUnit
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
@@ -12,15 +12,11 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Request.Builder
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.workmanager.dsl.worker
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val workerModule = module {
-    worker { DistanceTrackerService(get(), get(), androidContext(), get()) }
-}
 
 val mainModule = createMainModule() // UI level
 
@@ -59,4 +55,5 @@ private fun createMainModule() = module {
     }
     single { get<Retrofit>().create(FlickrService::class.java) }
     factory<NetworkSource> { RepositoriesSource() }
+    viewModel { TrackingViewModel(get()) }
 }
