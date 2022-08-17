dd03c2322241d3efd537ff13aa25a12d84cece26
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-17 10:21:45 +0200

    fix unit test for fetcher repository

--- data/src/test/kotlin/com/simplekjl/data/repository/ImageFetcherRepositoryImplTest.kt
+++ data/src/test/kotlin/com/simplekjl/data/repository/ImageFetcherRepositoryImplTest.kt
@@ -2,6 +2,7 @@ package com.simplekjl.data.repository
 
 import com.appmattus.kotlinfixture.kotlinFixture
 import com.google.common.truth.Truth.assertThat
+import com.simplekjl.data.client.FlickrService
 import com.simplekjl.data.util.MainCoroutineRule
 import com.simplekjl.data.util.runBlockingTest
 import com.simplekjl.domain.imagefetcher.ImageFetcherRepository
@@ -9,14 +10,16 @@ import com.simplekjl.domain.model.SearchPhotoPayload
 import com.simplekjl.domain.utils.Result
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-import io.mockk.coJustRun
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.unmockkAll
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import retrofit2.Response
 
 @ExperimentalCoroutinesApi
 internal class ImageFetcherRepositoryImplTest {
@@ -29,6 +32,9 @@ internal class ImageFetcherRepositoryImplTest {
     @MockK
     private lateinit var networkMockk: NetworkSource
 
+    @RelaxedMockK
+    private lateinit var service: FlickrService
+
     private lateinit var imageFetcherRepository: ImageFetcherRepository
 
     @Before
@@ -38,7 +44,7 @@ internal class ImageFetcherRepositoryImplTest {
     }
 
     @Test
-    fun `check imageFetcher returns error`() =
+    fun `check imageFetcher returns success`() =
         mainCoroutineRule.runBlockingTest {
             val lat = 34.0
             val lon = 34.0
@@ -49,10 +55,13 @@ internal class ImageFetcherRepositoryImplTest {
                     lon
                 )
             } returns Result.Success(photoData)
-            coJustRun { networkMockk.getGithubClient() }
+            every { networkMockk.getGithubClient() } returns service
+            coEvery {
+                networkMockk.getGithubClient().searchPhotoByLocation(lat, lon)
+            } returns Response.success(photoData)
 
             val result = imageFetcherRepository.getMatchingRepositories(lat, lon)
-            assertThat(result).isInstanceOf(Result.Error::class.java)
+            assertThat(result).isInstanceOf(Result.Success::class.java)
 
         }
 
