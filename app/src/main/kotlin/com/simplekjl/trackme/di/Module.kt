844ac36a777b232d5780b62dcf09f13df01d7d95
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-17 20:26:57 +0200

    updated files to integrate service

--- app/src/main/kotlin/com/simplekjl/trackme/di/Module.kt
+++ app/src/main/kotlin/com/simplekjl/trackme/di/Module.kt
@@ -1,10 +1,17 @@
 package com.simplekjl.trackme.di
 
+import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.LocationServices
 import com.simplekjl.data.client.FlickrService
 import com.simplekjl.data.repository.NetworkSource
 import com.simplekjl.trackme.BuildConfig
+import com.simplekjl.trackme.R
 import com.simplekjl.trackme.framework.RepositoriesSource
 import com.simplekjl.trackme.ui.trackimages.TrackingViewModel
+import com.simplekjl.trackme.utils.Constants.NOTIFICATION_CHANNEL_ID
 import java.util.concurrent.TimeUnit
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
@@ -12,6 +19,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Request.Builder
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -21,6 +29,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 val mainModule = createMainModule() // UI level
 
 
+@SuppressLint("UnspecifiedImmutableFlag")
 private fun createMainModule() = module {
     single {
         val builder = OkHttpClient().newBuilder()
@@ -55,5 +64,14 @@ private fun createMainModule() = module {
     }
     single { get<Retrofit>().create(FlickrService::class.java) }
     factory<NetworkSource> { RepositoriesSource() }
+    factory { params ->
+        NotificationCompat.Builder(params.get(), NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_mountain_icon)
+            .setContentIntent(params.get())
+    }
+    factory { LocationServices.getFusedLocationProviderClient(androidContext()) }
+    factory<NotificationManager> { androidContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
     viewModel { TrackingViewModel(get()) }
 }
--- app/src/main/kotlin/com/simplekjl/trackme/utils/GeneralFuncs.kt
+++ app/src/main/kotlin/com/simplekjl/trackme/utils/GeneralFuncs.kt
@@ -5,23 +5,16 @@ import com.google.maps.android.SphericalUtil
 import java.text.DecimalFormat
 
 object GeneralFuncs {
-    fun calculateElapsedTime(startTime: Long, stopTime: Long): String {
-        val elapsedTime = stopTime - startTime
-        val seconds = (elapsedTime / 1000) % 60
-        val minutes = (elapsedTime / (1000 * 60)) % 60
-        val hours = (elapsedTime / (1000 * 60 * 60)) % 24
-        return "$hours:$minutes:$seconds"
-    }
 
     fun calculateDistance(locationList: MutableList<LatLng>): String {
-
         if (locationList.size > 1) {
             val distance =
                 SphericalUtil.computeDistanceBetween(locationList.first(), locationList.last())
-            return DecimalFormat("#.##").format(distance / 1000)
+            val formatDistance = DecimalFormat("#.##").format(distance / 1000)
+            return formatDistance + "KM"
         }
 
-        return "0.00"
+        return "0.00KM"
 
     }
 }
