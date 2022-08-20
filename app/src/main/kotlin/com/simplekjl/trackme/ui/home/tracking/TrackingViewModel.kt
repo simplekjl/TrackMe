7be43deda0959002eb1b70f943b8e19218a8b0d7
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-20 04:56:53 +0200

    added viewmodel scope

--- app/src/main/kotlin/com/simplekjl/trackme/ui/home/tracking/TrackingViewModel.kt
+++ app/src/main/kotlin/com/simplekjl/trackme/ui/home/tracking/TrackingViewModel.kt
@@ -3,13 +3,14 @@ package com.simplekjl.trackme.ui.home.tracking
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.LatLng
 import com.simplekjl.domain.model.PhotoRaw
 import com.simplekjl.domain.usecases.GetImageUrlByLocationUseCase
 import com.simplekjl.domain.usecases.LocationDetails
 import com.simplekjl.domain.utils.Result.Error
 import com.simplekjl.domain.utils.Result.Success
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class TrackingViewModel(private val getImageUrlByLocationUseCase: GetImageUrlByLocationUseCase) :
@@ -21,7 +22,7 @@ class TrackingViewModel(private val getImageUrlByLocationUseCase: GetImageUrlByL
     fun shouldDownLoadImage(locationsList: MutableList<LatLng>, distance: Double) {
         if (locationsList.isEmpty()) return // return if it is empty
         if (kotlin.math.floor(distance) > (_imageList.value?.size ?: 0)) {
-            runBlocking {
+            viewModelScope.launch {
                 val result =
                     getImageUrlByLocationUseCase(
                         LocationDetails(
