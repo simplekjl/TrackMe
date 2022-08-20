06ec447141d435a7bea699d54127be7a59ac0797
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-08-20 04:18:44 +0200

    remove extensions

--- app/src/main/kotlin/com/simplekjl/trackme/extension/BooleanExtension.kt
+++
@@ -1,4 +0,0 @@
-package com.simplekjl.trackme.extension
-
-
-inline fun <reified T> Boolean.validate(accept: () -> T, denied: () -> T): T = if (this) accept() else denied()
--- app/src/main/kotlin/com/simplekjl/trackme/extension/LiveData.kt
+++
@@ -1,6 +0,0 @@
-package com.simplekjl.trackme.extension
-
-import androidx.lifecycle.MutableLiveData
-
-fun <T : Any> mutableLiveDataOf() = MutableLiveData<T>()
-fun <T : Any> mutableLiveDataOf(value: T) = MutableLiveData(value)
